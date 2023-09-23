dac27618de956e8d592b04017e219a8c490c94d5
imamhossainroni <imamhossainroni95@gmail.com>
2023-09-23 23:19:12 +0600

    🎉 main file for fastapi modified for project title and version

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.users.views import router as user_router
-app = FastAPI()
+from config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME, version="1.0.1")
 
 app.include_router(user_router, prefix="/users", tags=['User'])
